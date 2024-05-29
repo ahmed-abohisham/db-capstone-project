3b80beca94d1b077acfe9a12f3a12e160e67f6b7
Ahmed <ahm1976@gmail.com>
2024-05-29 20:15:15 +0300

    create procedure bookingudate

--- procedure-w2.sql
+++ procedure-w2.sql
@@ -108,3 +108,10 @@ select "New booking added" as "Confirmation";
 end //
 
 DELIMITER 
+
+
+CREATE DEFINER='admin1'@'%' PROCEDURE UpdateBooking(booking_id INT, booking_date DATE)
+BEGIN
+UPDATE bookings SET BookingSlot = booking_date WHERE BookingID = booking_id; 
+SELECT CONCAT("Booking", booking_id, "updated") AS "Confirmation";
+END
