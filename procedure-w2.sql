26e0d30673e97b5439bf5899fc443c1ab9dcbf76
Ahmed <ahm1976@gmail.com>
2024-05-29 18:20:15 +0300

    create procedure AddValidBooking

--- procedure-w2.sql
+++ procedure-w2.sql
@@ -50,3 +50,41 @@ END //
 
 DELIMITER ;
 
+
+
+
+DELIMITER //
+
+CREATE PROCEDURE AddValidBooking(
+    IN bookingDate DATE,
+    IN tableNumber INT,
+    IN customerName VARCHAR(255)
+)
+BEGIN
+    DECLARE @existingBookingCount INT;
+
+    -- Check if the table is already booked
+    SELECT COUNT(*) INTO @existingBookingCount
+    FROM bookings
+    WHERE BookingSlot = bookingDate AND TableNumber = tableNumber;
+
+      -- Start a transaction
+    START TRANSACTION;
+
+    IF @existingBookingCount > 0 THEN
+        -- The table is already booked, so rollback the transaction
+        ROLLBACK;
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Table is already booked for this date.';
+    ELSE
+        -- The table is available, so insert the new booking
+        INSERT INTO Bookings ( TableNumber,BookingSlot, CustomerName)
+        VALUES (bookingDate, tableNumber, customerName);
+
+        -- Commit the transaction
+        COMMIT;
+    END IF;
+END //
+
+DELIMITER ;
+
